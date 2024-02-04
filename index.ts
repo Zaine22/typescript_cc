66a036d11556af26f0e7ac3fd9c23c6220b0ec57
Zaine <57561694+Zaine22@users.noreply.github.com>
2024-02-04 07:49:14 +0630

    lesson 4

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-let age = 30;
+let age: number = 30;
+let firstName: string = "Zaine";
+let isFictional: boolean;
 
-console.log(age);
+age = 31;
+
+firstName = "Leo";
+
+isFictional = true;
+
+let planet = "Earth";
+let moons = 1;
+let isLarge = false;
+
+planet = "Saturn";
+moons = 145;
+isLarge = true;
+
+let something: null;
+let anotherThing: undefined;
+
+something = null;
+anotherThing = undefined;
